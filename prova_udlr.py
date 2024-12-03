e141797de67747cc2bf2493540f9d7e6ca9903e4
Allegra Strippoli <allegra@strippoli.eu>
2024-12-03 15:28:43 +0100

    vincoli sugli angoli

--- prova_udlr.py
+++ prova_udlr.py
@@ -3,6 +3,20 @@ from gurobipy import GRB
 from graph import Graph
 import gurobipy as gp
 
+def check_angles(model, cycle, variable_matrix, i, angle_matrix, direction1, direction2, angle, triggers):
+        print(cycle[i], cycle[(i + 1) % len(cycle)], cycle[(i + 2) % len(cycle)])
+        x = variable_matrix[cycle[i]][cycle[(i + 1) % len(cycle)]][direction1] 
+        y = variable_matrix[cycle[(i + 1) % len(cycle)]][cycle[(i + 2) % len(cycle)]][direction2]
+
+        trigger = model.addVar(vtype=GRB.BINARY, name=f"trigger_{i}_{cycle}")
+        triggers.append(trigger)
+
+        model.addConstr((trigger == 1) >> (x + y >= 2))
+        model.addConstr((trigger == 0) >> (x + y <= 1))
+
+        model.addConstr((trigger == 1) >> (angle_matrix[cycle[(i + 1) % len(cycle)]][angle] == 1))
+        model.addConstr((trigger == 0) >> (angle_matrix[cycle[(i + 1) % len(cycle)]][angle] == 0))
+
 def check_graph_udlr(g: Graph):
         model = gp.Model("m1")
         
@@ -31,54 +45,63 @@ def check_graph_udlr(g: Graph):
                 # an edge between nodes i and j can only have one direction
                 model.addConstr(sum(variable_matrix[i][j][k] for k in range(4)) == 1) 
 
-                
+        angle_list = [(0, 2, 1), (2, 1, 3), (1, 3, 2), (3, 0, 0)]   
+        
         for cycle in g.find_all_cycles():
-            model.addConstr(sum(variable_matrix[cycle[(i + 1) % len(cycle)]][cycle[i]][0] for i in range(len(cycle))) >= 1) 
-            model.addConstr(sum(variable_matrix[cycle[(i + 1) % len(cycle)]][cycle[i]][1] for i in range(len(cycle))) >= 1) 
-            model.addConstr(sum(variable_matrix[cycle[(i + 1) % len(cycle)]][cycle[i]][2] for i in range(len(cycle))) >= 1) 
-            model.addConstr(sum(variable_matrix[cycle[(i + 1) % len(cycle)]][cycle[i]][3] for i in range(len(cycle))) >= 1) 
+            print(cycle)
+            if len(cycle) == 3: continue
+            # check clockwise if there is at least one edge in each direction 
+            # clockwise => variable_matrix[i][j] is checked, while variable_matrix[j][i] is not
+            model.addConstr(sum(variable_matrix[cycle[i]][cycle[(i + 1) % len(cycle)]][0] for i in range(len(cycle))) >= 1) 
+            model.addConstr(sum(variable_matrix[cycle[i]][cycle[(i + 1) % len(cycle)]][1] for i in range(len(cycle))) >= 1) 
+            model.addConstr(sum(variable_matrix[cycle[i]][cycle[(i + 1) % len(cycle)]][2] for i in range(len(cycle))) >= 1) 
+            model.addConstr(sum(variable_matrix[cycle[i]][cycle[(i + 1) % len(cycle)]][3] for i in range(len(cycle))) >= 1) 
             
-  
-            
-            for i in range(len(cycle)):
-                model.addConstr(sum(variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][0] + variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][1] for j in range(i, i+len(cycle)-2)) >= 1) # at least one left or right each n-2 edges
+            # for i in range(len(cycle)):
+            #     model.addConstr(sum(variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][0] + variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][1] for j in range(i, i+len(cycle)-2)) >= 1) # at least one horizontal each n-2 edges
                 
-                model.addConstr(sum(variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][2] + variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][3] for j in range(i, i+len(cycle)-2)) >= 1)
+            #     model.addConstr(sum(variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][2] + variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][3] for j in range(i, i+len(cycle)-2)) >= 1) # at least one vertical 
                 
-                model.addConstr(sum(variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][0] + variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][1] for j in range(i, i+len(cycle)-2)) <= len(cycle)-3) 
+            #     model.addConstr(sum(variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][0] + variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][1] for j in range(i, i+len(cycle)-2)) <= len(cycle)-3) # at most len(cycle)-2 horizontal
                 
-                model.addConstr(sum(variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][2] + variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][3] for j in range(i, i+len(cycle)-2)) <= len(cycle)-3)
+            #     model.addConstr(sum(variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][2] + variable_matrix[cycle[j % len(cycle)]][cycle[(j + 1) % len(cycle)]][3] for j in range(i, i+len(cycle)-2)) <= len(cycle)-3) # at most len(cycle)-2 vertical
                 
             angle_matrix = [[None for _ in range(4)] for _ in range(len(g.adjacency_list))]
+            
             for i in range(len(g.adjacency_list)):
                 angle_matrix[i][0] = model.addVar(vtype=GRB.BINARY, name=f"{i}_left_up")
                 angle_matrix[i][1] = model.addVar(vtype=GRB.BINARY, name=f"{i}_right_up")
                 angle_matrix[i][2] = model.addVar(vtype=GRB.BINARY, name=f"{i}_left_down")
                 angle_matrix[i][3] = model.addVar(vtype=GRB.BINARY, name=f"{i}_right_down")
                 
+        triggers = []
         for i in range(len(cycle)):
-            x = variable_matrix[cycle[(i + 1) % len(cycle)]][cycle[i]][0]
-            y = variable_matrix[cycle[(i + 1) % len(cycle)]][cycle[(i + 2) % len(cycle)]][2]
-
-            trigger = model.addVar(vtype=GRB.BINARY, name=f"trigger_{i}")
-
-            model.addConstr((trigger == 1) >> (x + y == 2))
-            model.addConstr((trigger == 0) >> (x + y <= 1))
-
-            model.addConstr((trigger == 1) >> (angle_matrix[cycle[(i + 1) % len(cycle)]][0] == 1))
-            model.addConstr((trigger == 0) >> (angle_matrix[cycle[(i + 1) % len(cycle)]][0] == 0))
-
-        model.addConstr(sum(angle_matrix[cycle[i]][0] for i in range(len(cycle))) >= 1)
+            for angle in angle_list:
+                check_angles(model, cycle, variable_matrix, i, angle_matrix, angle[0], angle[1], angle[2], triggers)
+                model.addConstr(sum(angle_matrix[i][k] for k in range(4)) <= 1) 
             
+        model.addConstr(sum(angle_matrix[cycle[i]][0] for i in range(len(cycle))) >= 1)
+        model.addConstr(sum(angle_matrix[cycle[i]][1] for i in range(len(cycle))) >= 1)
+        model.addConstr(sum(angle_matrix[cycle[i]][2] for i in range(len(cycle))) >= 1)
+        model.addConstr(sum(angle_matrix[cycle[i]][3] for i in range(len(cycle))) >= 1)
+        
         model.optimize()
         
+        # for trigger in triggers:
+        #     print(trigger.varName, trigger.X)
+            
+        # for cycle in g.find_all_cycles():
+        #     for i in range(len(cycle)):
+        #                 print(f"node {i} = {angle_matrix[i][0].x}, {angle_matrix[i][1].x}, {angle_matrix[i][2].x}, {angle_matrix[i][3].x}")
+            
         # for i in range(len(g.adjacency_list)):
         #     for j in g.get_neighbors(i):
-        #         if(i < j):
+        #         # if(i < j):
         #             print(f"{i}_{j} = {variable_matrix[i][j][0].x}, {variable_matrix[i][j][1].x}, {variable_matrix[i][j][2].x}, {variable_matrix[i][j][3].x}")
         
         if model.status == GRB.OPTIMAL:
                 print("Solution found.")
+    
                 for var in model.getVars():
                     if var.X > 0:   
                         print('%s %g' % (var.VarName, var.X))
