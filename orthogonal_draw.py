9c1672ae8e8016ee3661d186c0e62a177983b57e
friscobuffo <gio.andreola@stud.uniroma3.it>
2024-12-05 16:53:57 +0100

    commit

--- orthogonal_draw.py
+++ orthogonal_draw.py
@@ -8,7 +8,10 @@ def make_orthogonal_draw(graph: Graph):
     shape = build_shape(graph)
     if shape:
         nodes_positions = shape_to_nodes_positions(graph, shape)
-        nodes_positions_to_drawing(graph, nodes_positions)
+        if nodes_positions:
+            nodes_positions_to_drawing(graph, nodes_positions)
+        else:
+            print("error: admissible shape but drawing not found")
 
 if __name__ == "__main__":
     for i in all_example_graphs_indexes():
